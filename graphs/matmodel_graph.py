af64d3d3c92b0738d794cb69c67b2240a71ed1f6
Artyom <fantastik.play.2000@gmail.com>
2023-12-28 11:44:01 +0300

    fix

--- graphs/matmodel_graph.py
+++ graphs/matmodel_graph.py
@@ -1,37 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import integrate, constants
+from scipy import integrate
+from scipy import constants
 
 # данные
-m0 = 28000  # масса без топлива
+m0 = 28000 #масса без топлива
 M = 63150  # масса с топливом
-Cf = 0.5  # сопротивление
+Cf = 0.5 #сопротивление
 ro = 1.293  # плотность воздуха
-S = constants.pi * ((1.3 / 2) ** 2)  # площадь сечения
+S = constants.pi * ((0.7 / 2) ** 2) #площадь сечения
 g = constants.g
-F = [1571000, 388000]
+F = [1579000, 588000]
+
 
 
 def dv_dt(t, v):
-    if t < 30:
+    if t < 90:
         M = 63150
         m0 = 28000
         Ft = F[0]
-        T = 4.66 * 60
-        k = (M - m0) / T
-        return ((Ft / (M - k * t)) + ((Cf * ro * S) / (2 * (M - k * t))) * v ** 2 - g)
-    if t < 105:
-        M = 40000
-        m0 = 18000
+        k = (M - m0) / (4.66 * 60)
+        return ((Ft / (M - k * t)) - ((Cf * ro * S) / (2 * (M - k * t))) * v ** 2 - g)
+    if t < 305:
+        M = 42000
+        m0 = 16000
         Ft = F[1]
-        T = 5.17 * 60
-        k = (M - m0) / T
-        return ((Ft / (M - k * t)) + ((Cf * ro * S) / (2 * (M - k * t))) * v ** 2 - g)
+        k = (M - m0) / (5.2 * 60)
+        return ((Ft / (M - k * t)) - ((Cf * ro * S) / (2 * (M - k * t))) * v ** 2 - g)
+
 
 
 v0 = 0
 
-t = np.linspace(0, 300, 400) # 400 чисел, равномерно распределенных между 0 и 300
+t = np.linspace(0, 335, 315)
 
 solve = integrate.solve_ivp(dv_dt, t_span=(0, max(t)), y0=[v0], t_eval=t)
 
